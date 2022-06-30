ecc1ad08f1d4a385c5be4f735066de7579f79864
fwio <624835339@qq.com>
2022-06-30 11:46:10 +0800

    Add socket.io handlers.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { Chatrecord } from './entity/ChatRecord'
 import * as express from 'express'
 import { Server } from 'socket.io'
 import { AddressInfo } from 'net'
@@ -19,6 +20,7 @@ import orderAPI from './routes/User/order'
 import securityAPI from './routes/User/security'
 import cartAPI from './routes/User/shoppingcart'
 import tradeAPI from './routes/User/trade'
+import { AppDataSource } from './data-source'
 
 const app = express()
 
@@ -50,24 +52,32 @@ const server = app.listen(8082, () => {
   console.log('Server is running at http://%s:%s', host, port)
 })
 
-/* // 配置 Socket.io
+// 配置 Socket.io
 const io = new Server({
   cors: {
     origin: 'http://106.15.78.201:8084'
   }
 }).listen(server)
 
+interface Message {
+  a_user_id: string
+  b_user_id: string
+  speaker: number
+  date_time: string
+  details: string
+}
+
 // WebSocket 连接桶
 const sessionBucket = new Map<string, Set<string>>()
 
 io.on('connection', (socket) => {
   // 建立 WebSocket 连接时，将用户ID和socket id加入map中，以便广播
-  let userID = socket.handshake.headers.userid
+  const userID = socket.handshake.headers.userid
   // 获取用户ID对应的连接集合
   let userSockets = sessionBucket.get(userID as string)
   if (!userSockets) {
     // 如果桶中不存在该用户对应的连接，则新建集合
-    sessionBucket.set(userID as string, userSockets = new Set([socket.id]))
+    sessionBucket.set(userID as string, userSockets = new Set([ socket.id ]))
   } else {
     // 桶中存在该用户对应的连接集合，将 socket 连接加入集合
     userSockets.add(socket.id)
@@ -75,28 +85,33 @@ io.on('connection', (socket) => {
   //console.log(`connect: ${socket.id}, userID: ${socket.handshake.headers.userid}`)
 
   // 用户发送消息
-  socket.on('send message', (msg: Message) => {
+  socket.on('send message', async (msg: Message) => {
     // 调试：打印消息
     //console.log(msg)
 
     // 将消息加入数据库
-    let newMessage = [msg.a_user_id, msg.b_user_id, msg.speaker, msg.date_time, msg.details]
-    connection.query(
-      "insert into chatRecord(a_user_id,b_user_id,speaker,date_time,details) value(?,?,?,?,?)",
-      newMessage,
-      (err, result) => {
-        if (err) throw err
-        // 广播消息（仅针对消息双方）
-        // 获取消息双方ID建立的所有socket连接
-        const aSockets = sessionBucket.get(msg.a_user_id)
-        const bSockets = sessionBucket.get(msg.b_user_id)
-        const relatedSockets = [...(aSockets ? Array.from(aSockets) : []), ...(bSockets ? Array.from(bSockets) : [])]
-        if (relatedSockets.length) {
-          // 向相关连接发送消息
-          io.to(relatedSockets).emit('deliver message', msg)
-        }
-      }
-    )
+    await AppDataSource
+      .createQueryBuilder()
+      .insert()
+      .into(Chatrecord)
+      .values({
+        aUserId: msg.a_user_id,
+        bUserId: msg.b_user_id,
+        speaker: msg.speaker,
+        dateTime: msg.date_time,
+        details: msg.details
+      })
+      .execute()
+
+    // 广播消息（仅针对消息双方）
+    // 获取消息双方ID建立的所有socket连接
+    const aSockets = sessionBucket.get(msg.a_user_id)
+    const bSockets = sessionBucket.get(msg.b_user_id)
+    const relatedSockets = [ ...(aSockets ? Array.from(aSockets) : []), ...(bSockets ? Array.from(bSockets) : []) ]
+    if (relatedSockets.length) {
+      // 向相关连接发送消息
+      io.to(relatedSockets).emit('deliver message', msg)
+    }
   })
 
   socket.on('disconnect', () => {
@@ -104,4 +119,4 @@ io.on('connection', (socket) => {
     (userSockets as Set<string>).delete(socket.id)
     //console.log(`disconnect: ${socket.id}`)
   })
-}) */
+})
